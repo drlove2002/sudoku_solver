09c238d8c60d57d08c17d7dfeb0363c36cc9004c
Sudip Roy <sudiproy20yo@gmail.com>
2025-12-05 11:46:40 +0530

    Update Nix configuration

--- flake.nix
+++ flake.nix
@@ -1,106 +1,28 @@
 {
-  description = "Python + Rust project";
+  description = "Rust package";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-outputs = inputs:
-    inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ inputs.rust-overlay.overlays.default ];
-        };
-        lib = pkgs.lib;
+  outputs =
+    { self, nixpkgs }:
+    let
+      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+    in
+    {
 
-        # Get a custom rust toolchain
-        customRustToolchain = pkgs.rust-bin.stable."1.70.0".default;
-        craneLib =
-          (inputs.crane.mkLib pkgs).overrideToolchain customRustToolchain;
+      devShells."x86_64-linux".default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          gcc
+          rustc
+          cargo
+          rustfmt
+          clippy
+          rust-analyzer
+        ];
+        env.RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+      };
 
-        projectName =
-          (craneLib.crateNameFromCargoToml { cargoToml = ./Cargo.toml; }).pname;
-        projectVersion = (craneLib.crateNameFromCargoToml {
-          cargoToml = ./Cargo.toml;
-        }).version;
-
-        pythonVersion = pkgs.python310;
-        wheelTail =
-          "cp310-cp310-manylinux_2_34_x86_64"; # Change if pythonVersion changes
-        wheelName = "${projectName}-${projectVersion}-${wheelTail}.whl";
-
-        crateCfg = {
-          src = craneLib.cleanCargoSource (craneLib.path ./.);
-          nativeBuildInputs = [ pythonVersion ];
-        };
-
-        # Build the library, then re-use the target dir to generate the wheel file with maturin
-        crateWheel = (craneLib.buildPackage (crateCfg // {
-          pname = projectName;
-          version = projectVersion;
-          # cargoArtifacts = crateArtifacts;
-        })).overrideAttrs (old: {
-          nativeBuildInputs = old.nativeBuildInputs ++ [ pkgs.maturin ];
-          buildPhase = old.buildPhase + ''
-            maturin build --offline --target-dir ./target
-          '';
-          installPhase = old.installPhase + ''
-            cp target/wheels/${wheelName} $out/
-          '';
-        });
-      in rec {
-        packages = rec {
-          default = crateWheel; # The wheel itself
-
-          # A python version with the library installed
-          pythonEnv = pythonVersion.withPackages
-            (ps: [ (lib.pythonPackage ps) ] ++ (with ps; [ ipython ]));
-        };
-
-        lib = {
-          # To use in other builds with the "withPackages" call
-          pythonPackage = ps:
-            ps.buildPythonPackage rec {
-              pname = projectName;
-              format = "wheel";
-              version = projectVersion;
-              src = "${crateWheel}/${wheelName}";
-              doCheck = false;
-              pythonImportsCheck = [ projectName ];
-            };
-        };
-
-        devShells = rec {
-          rust = pkgs.mkShell {
-            name = "rust-env";
-            src = ./.;
-            nativeBuildInputs = with pkgs; [ pkg-config rust-analyzer maturin ];
-          };
-          python = let
-          in pkgs.mkShell {
-            name = "python-env";
-            src = ./.;
-            nativeBuildInputs = [ packages.pythonEnv ];
-          };
-          default = rust;
-        };
-
-        apps = rec {
-          ipython = {
-            type = "app";
-            program = "${packages.pythonEnv}/bin/ipython";
-          };
-          default = ipython;
-        };
-      });
+    };
 }
