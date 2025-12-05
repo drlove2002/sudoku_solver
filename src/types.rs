ec5fbfa4375b6eb8991464e7741dabef3e2561ce
Sudip Roy <sudiproy20yo@gmail.com>
2025-12-05 11:45:07 +0530

    Refactor Board to use const generics

--- src/types.rs
+++ src/types.rs
@@ -1,22 +1,32 @@
-#[allow(dead_code)]
-use serde::{Deserialize, Serialize};
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct Board {
-    pub size: usize,
-    pub cells: Vec<u8>,
+#[derive(Debug, Clone)]
+pub struct Board<const N: usize> {
+    pub cells: [[u8; N]; N],
 }
 
-impl Board {
+impl<const N: usize> Board<N> {
+    pub fn size(&self) -> usize {
+        N
+    }
+
+    pub fn n(&self) -> usize {
+        (N as f64).sqrt() as usize
+    }
+
+    pub fn get_cell(&self, idx: usize) -> u8 {
+        let r = idx / N;
+        let c = idx % N;
+        self.cells[r][c]
+    }
+
     pub fn is_valid(&self) -> bool {
-        let n = (self.size as f64).sqrt() as usize;
+        let root_n = self.n();
         // Check rows and columns
-        for i in 0..self.size {
-            let mut row_seen = vec![false; self.size + 1];
-            let mut col_seen = vec![false; self.size + 1];
-            for j in 0..self.size {
-                let row_val = self.cells[i * self.size + j];
-                let col_val = self.cells[j * self.size + i];
+        for i in 0..N {
+            let mut row_seen = vec![false; N + 1];
+            let mut col_seen = vec![false; N + 1];
+            for j in 0..N {
+                let row_val = self.cells[i][j];
+                let col_val = self.cells[j][i];
                 if row_val != 0 {
                     if row_seen[row_val as usize] {
                         return false;
@@ -32,12 +42,12 @@ impl Board {
             }
         }
         // Check minigrids
-        for box_row in 0..n {
-            for box_col in 0..n {
-                let mut box_seen = vec![false; self.size + 1];
-                for i in 0..n {
-                    for j in 0..n {
-                        let val = self.cells[(box_row * n + i) * self.size + (box_col * n + j)];
+        for box_row in 0..root_n {
+            for box_col in 0..root_n {
+                let mut box_seen = vec![false; N + 1];
+                for i in 0..root_n {
+                    for j in 0..root_n {
+                        let val = self.cells[box_row * root_n + i][box_col * root_n + j];
                         if val != 0 {
                             if box_seen[val as usize] {
                                 return false;
